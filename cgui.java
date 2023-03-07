919e3e36890cc51dde9c54ed752124e9c9cd0670
burneajohnpaul <124126034+burneajohnpaul@users.noreply.github.com>
2023-03-07 18:28:06 +0800

    Add files via upload

---
+++ cgui.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jong's TV
+ */
+public class cgui {
+    String pname; 
+    String ptype;
+    String pemploy;
+    
+    public cgui(String pname, String ptype, String pemploy){
+        this.pname = pname;
+        this.ptype = ptype;
+        this.pemploy = pemploy;
+    }
+    
+}
